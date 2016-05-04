ddbb81423ec7aa4eb36e09682718f3346951ec76
Dmitriy Kovalenko <runningmaster@gmail.com>
2016-05-04 23:18:35 +0300

    make: add -i to build

--- src/make.bash
+++ src/make.bash
@@ -2,5 +2,5 @@
 . ../etc/env.conf
 
 go generate internal/version
-go build -o $GOPATH/bin/$PROJNAME main
+go build -i -o $GOPATH/bin/$PROJNAME main
 go list ./... | grep -v vendor/ | xargs -L1 go fmt
