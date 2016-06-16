21fe4afae4221a3dbeed4be51c018f9c435beba9
Dmitriy Kovalenko <runningmaster@gmail.com>
2016-06-16 13:39:39 +0300

    conf: remove package

--- src/main/main.go
+++ src/main/main.go
@@ -1,35 +1,19 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
 	"os"
 
 	"internal/cmd"
-	"internal/conf"
 
 	"github.com/google/subcommands"
 	"golang.org/x/net/context"
 )
 
-func init() {
-	initConfig()
-	initLogger()
-}
-
 func main() {
+	log.SetFlags(0)
+	log.SetOutput(os.Stderr)
+
 	cmd.Register()
 	os.Exit(int(subcommands.Execute(context.Background())))
-}
-
-func initConfig() {
-	conf.Parse()
-}
-
-func initLogger() {
-	log.SetFlags(0)
-	log.SetOutput(ioutil.Discard)
-	if conf.Verbose {
-		log.SetOutput(os.Stderr)
-	}
 }
