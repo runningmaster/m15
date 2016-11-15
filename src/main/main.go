912f149a362ea2900aaab4d4f51d7e478b9fcbef
Dmitriy Kovalenko <runningmaster@gmail.com>
2016-11-15 13:41:04 +0200

    main: add global flags

--- src/main/main.go
+++ src/main/main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
 	"log"
 	"os"
 
 	"internal/cli"
 )
 
+var (
+	flagVerbose  = flag.Bool("verbose", false, "make logging visible")
+	flagHideTime = flag.Bool("hidetime", false, "show time when verbose")
+)
+
 func main() {
 	flag.Parse()
-
-	log.SetFlags(0)
-	log.SetOutput(os.Stderr)
-
+	initLogger(*flagVerbose, *flagHideTime)
 	os.Exit(cli.Run())
+}
+
+func initLogger(v, ht bool) {
+	log.SetOutput(ioutil.Discard)
+	if v {
+		log.SetOutput(os.Stderr)
+	}
+	if ht {
+		log.SetFlags(0)
+	}
 }
