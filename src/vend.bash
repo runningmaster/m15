3b6dae108ed07d4acbdbea168e832fae56bfb82e
Dmitriy Kovalenko <runningmaster@gmail.com>
2017-02-12 19:16:56 +0200

    src: improve vend.bash

--- src/vend.bash
+++ src/vend.bash
@@ -2,27 +2,27 @@
 . ../etc/env.conf
 
 confirm "Update vendor packages?" || exit 0
-VENDOR=$GOPATH/src/vendor
+vendor=$GOPATH/src/vendor
 
 # cleanup
-find $VENDOR -mindepth 1 -maxdepth 1 -type d | xargs rm -rf
+find $vendor -mindepth 1 -maxdepth 1 -type d | xargs rm -rf
 
 # download
-GOPATH=$VENDOR
+GOPATH=$vendor
 while read pkg
 do
 	[[ $pkg =~ ^#.*$ || $pkg = "" ]] && continue
 	go get -d $pkg
 	echo " ->" $pkg "- OK"
-done < vendor.ls
+done < vend.list
 
 # move src
-if [ -d "$VENDOR/src" ]; then
-	cp -rf $VENDOR/src/* $VENDOR  && rm -rf $VENDOR/src
+if [ -d "$vendor/src" ]; then
+	cp -rf $vendor/src/* $vendor  && rm -rf $vendor/src
 fi
 
 # save git rev
-find $VENDOR -type d -name ".git" -print0 | while IFS= read -r -d $'\0' line; do
+find $vendor -type d -name ".git" -print0 | while IFS= read -r -d $'\0' line; do
 	cd $(dirname "$line") && data=$(git --no-pager log -n 1 --format=format:"%H,%cd,%an <%ce>" HEAD)
 	IFS=","
 	for x in $data
@@ -34,5 +34,5 @@ done
 # kill .git dir
 for i in ".git*" ".hg*" ".bzr*" ".svn*"
 do
-	find $VENDOR -type d -name "$i" -print0 | xargs -0 rm -rf
+	find $vendor -type d -name "$i" -print0 | xargs -0 rm -rf
 done
