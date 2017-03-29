59be41b6c6729fa06f9d6188fa29f554e1b7ee13
Dmitriy Kovalenko <runningmaster@gmail.com>
2017-03-29 16:50:39 +0300

    src: improve make.bash

--- src/make.bash
+++ src/make.bash
@@ -3,4 +3,5 @@
 
 go list ./... | grep -v vendor/ | xargs -L1 go generate
 go list ./... | grep -v vendor/ | xargs -L1 go fmt
-go build -i -o $GOPATH/bin/$PROJNAME main
+go install -ldflags "-s -w" main
+mv -f $GOPATH/bin/main $GOPATH/bin/$PROJNAME
