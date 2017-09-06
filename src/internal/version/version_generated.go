a05642446eaa2be9df77a9384ab920880f0e1f46
Dmitriy Kovalenko <runningmaster@gmail.com>
2017-09-06 12:13:36 +0300

    run: fix a24 (2)

--- src/internal/version/version_generated.go
+++ src/internal/version/version_generated.go
@@ -18,8 +18,8 @@ const (
 	PreRelease = prerelease
 
 	// BuildTime is build metadata and it may be denoted by appending a plus sign and a series of dot separated identifiers immediately following the patch or pre-release version.
-	BuildTime = "20170906120400"
+	BuildTime = "20170906121306"
 
 	// GitCommit is build metadata and it may be denoted by appending a plus sign and a series of dot separated identifiers immediately following the patch or pre-release version.
-	GitCommit = "be4b5bc3"
+	GitCommit = "eb937569"
 )
